c36ed6df7451807fd360c8f292751c75ab371ab8
John Demetrio <johndemetrio123@gmail.com>
2024-04-19 09:47:22 -0300

    add: conteudo do curso modulo 1

---
+++ memoteca/src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamentos',
+  templateUrl: './criar-pensamentos.component.html',
+  styleUrls: ['./criar-pensamentos.component.css']
+})
+export class CriarPensamentosComponent implements OnInit {
+
+  pensamento ={
+    id: '1',
+    conteudo: 'Teste app alura',
+    autoria: 'jojohn',
+    modelo: ''
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
